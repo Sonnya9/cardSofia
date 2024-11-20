b458df9b009dc42bc800a4a6c9a5a0ee26d636e5
LenziFire1 <beregovenkosofia@gmail.com>
2024-11-20 18:27:02 +0200

    create css

---
+++ script.js
@@ -0,0 +1,15 @@
+let git = document.querySelector('.gitHub');
+let inst = document.querySelector('.inst');
+let tg = document.querySelector('.tg');
+
+git.addEventListener('click', function(){
+    location.assign('https://github.com/Sonnya9')
+});
+
+inst.addEventListener('click', function() {
+    location.assign('https://www.instagram.com/')
+});
+
+tg.addEventListener('click', function(){
+    location.assign('https://github.com/Sonnya9')
+});
